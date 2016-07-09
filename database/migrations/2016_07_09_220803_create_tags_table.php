14ff48767a42f8ae66964cecd2359834bf5c1dcc
Marius van Zundert <marius@ezcompany.nl>
2016-07-10 00:15:41 +0200

    mvz-blog | Added tags pivot migrations and updated tag migration

--- database/migrations/2016_07_09_220803_create_tags_table.php
+++ database/migrations/2016_07_09_220803_create_tags_table.php
@@ -5,26 +5,29 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateTagsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('tags', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   */
+  public function up()
+  {
+    Schema::create('tags', function (Blueprint $table) {
+      $table->increments('id');
+      $table->string('tag')->unique();
+      $table->string('title');
+      $table->string('subtitle');
+      $table->string('page_image');
+      $table->string('meta_description');
+      $table->string('layout')->default('blog.layouts.index');
+      $table->boolean('reverse_direction');
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::drop('tags');
-    }
+  /**
+   * Reverse the migrations.
+   */
+  public function down()
+  {
+    Schema::drop('tags');
+  }
 }
