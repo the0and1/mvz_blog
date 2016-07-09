0f15949130a16b70ed8c9938a5995e4b5bf078af
Marius van Zundert <marius@ezcompany.nl>
2016-07-09 23:12:10 +0200

    mvz-blog | Added database factories and seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,17 +5,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        Model::unguard();
+  /**
+   * Run the database seeds.
+   */
+  public function run()
+  {
+    Model::unguard();
 
-        // $this->call(UserTableSeeder::class);
+    $this->call('PostTableSeeder');
+  }
 
-        Model::reguard();
-    }
+}
+
+class PostTableSeeder extends Seeder
+{
+  public function run()
+  {
+    App\Post::truncate();
+
+    factory(App\Post::class, 20)->create();
+  }
 }
