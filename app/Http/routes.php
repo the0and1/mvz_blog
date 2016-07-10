ebda7fb7d98abd930feec4755da4e6094ec65dd5
Marius van Zundert <marius@ezcompany.nl>
2016-07-10 03:57:42 +0200

    mvz-blog | Updated routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -31,10 +31,10 @@ $router->group([
   'namespace' => 'Admin',
   'middleware' => 'auth',
 ], function () {
-  resource('admin/post', 'PostController');
+  resource('admin/post', 'PostController', ['except' => 'show']);
   resource('admin/tag', 'TagController', ['except' => 'show']);
   get('admin/upload', 'UploadController@index');
-  
+
   post('admin/upload/file', 'UploadController@uploadFile');
   delete('admin/upload/file', 'UploadController@deleteFile');
   post('admin/upload/folder', 'UploadController@createFolder');
