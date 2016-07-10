57d4b811876d0e11acabad3265c81a71371fe4dc
Marius van Zundert <marius@ezcompany.nl>
2016-07-10 04:11:06 +0200

    mvz-blog | Updated partials

--- resources/views/blog/partials/page-nav.php
+++ resources/views/blog/partials/page-nav.php
@@ -1,7 +1,7 @@
-{{-- Navigation --}}
+<!--{{-- Navigation --}}-->
 <nav class="navbar navbar-default navbar-custom navbar-fixed-top">
   <div class="container-fluid">
-    {{-- Brand and toggle get grouped for better mobile display --}}
+<!--    {{-- Brand and toggle get grouped for better mobile display --}}-->
     <div class="navbar-header page-scroll">
       <button type="button" class="navbar-toggle" data-toggle="collapse"
               data-target="#navbar-main">
@@ -10,10 +10,9 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="/">{{ config('blog.name') }}</a>
     </div>
 
-    {{-- Collect the nav links, forms, and other content for toggling --}}
+<!--    {{-- Collect the nav links, forms, and other content for toggling --}}-->
     <div class="collapse navbar-collapse" id="navbar-main">
       <ul class="nav navbar-nav">
         <li>
